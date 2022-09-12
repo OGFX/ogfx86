108db3355f8b53c4201789ec737911b367454201
Florian Paul Schmidt <mista.tapas@gmx.net>
2022-09-12 14:00:18 +0200

    optimized build

--- configuration.nix
+++ configuration.nix
@@ -5,6 +5,14 @@ let
   ogfx-ui = (pkgs.python39Packages.callPackage ./ogfx-ui.nix { ogfx-tools = ogfx-tools; });
   state-variable-filter-lv2 = (pkgs.callPackage ./state-variable-filter-lv2.nix {});
   clipping-lv2 = (pkgs.callPackage ./clipping-lv2.nix {});
+  # optimized_pkgs = import <nixpkgs> {
+  #   localSystem = {
+  #     gcc.arch = "westmere";
+  #     gcc.tune = "westmere";
+  #     system = "x86_64-linux";
+  #   };
+  # };
+  optimized_pkgs = pkgs;
 in
 {
   imports =
@@ -12,6 +20,14 @@ in
       ./hardware-configuration.nix
       ./musnix
     ];
+
+  nix.settings.system-features = [ "big-parallel" "nixos-test" "kvm" "benchmark" "gccarch-westmere" ];
+
+  nixpkgs.localSystem = {
+    gcc.arch = "westmere";
+    gcc.tune = "westmere";
+    system = "x86_64-linux";
+  };
 
   musnix = {
     enable = true;
@@ -58,28 +74,35 @@ in
   i18n.defaultLocale = "en_US.UTF-8";
   time.timeZone = "Europe/Berlin";
 
-  environment.systemPackages = with pkgs; [
-    vim wget htop links2 git tmux
-    schedtool usbutils psmisc
-    lm_sensors
-
-    dmenu arandr xfce.xfce4-terminal firefox
-    man-pages man-pages-posix
-  ] ++ [
-    jalv lv2 lilv ingen carla guitarix plugin-torture
-  ] ++ [
-    ogfx-ui state-variable-filter-lv2 clipping-lv2
-  ] ++ [
-    swh_lv2 mda_lv2 ams-lv2 aether-lv2
-    gxplugins-lv2 gxmatcheq-lv2 airwindows-lv2
-    rkrlv2 distrho bshapr bchoppr tunefish
-    plujain-ramp mod-distortion x42-plugins
-    infamousPlugins mooSpace boops
-    eq10q talentedhack artyFX fverb
-    kapitonov-plugins-pack fomp molot-lite
-    zam-plugins lsp-plugins calf gxmatcheq-lv2
-  ];
-
+  environment.systemPackages = 
+    (with pkgs; [
+      vim wget htop links2 git tmux
+      schedtool usbutils psmisc
+      lm_sensors
+  
+      dmenu arandr xfce.xfce4-terminal firefox
+      man-pages man-pages-posix
+    ]) 
+    ++ 
+    (with optimized_pkgs; [
+      jalv lv2 lilv ingen carla guitarix plugin-torture
+    ] 
+    ++ 
+    [
+      ogfx-ui state-variable-filter-lv2 clipping-lv2
+    ] 
+    ++ 
+    [
+      swh_lv2 mda_lv2 ams-lv2 aether-lv2
+      gxplugins-lv2 gxmatcheq-lv2 airwindows-lv2
+      rkrlv2 distrho bshapr bchoppr tunefish
+      plujain-ramp mod-distortion x42-plugins
+      infamousPlugins mooSpace boops
+      eq10q talentedhack artyFX fverb
+      kapitonov-plugins-pack fomp molot-lite
+      zam-plugins lsp-plugins calf gxmatcheq-lv2
+    ]);
+  
   sound.enable = true;
 
   services = {
