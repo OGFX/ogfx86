dff2056b207f97a243e414001afbb340c7255bdf
Florian Paul Schmidt <mista.tapas@gmx.net>
2022-09-14 21:05:03 +0200

    nix is still weird sometimes

--- ogfx-packages.nix
+++ ogfx-packages.nix
@@ -6,7 +6,7 @@
       mod-host = (pkgs.callPackage ./mod-host.nix {});
       mod-utilities = (pkgs.callPackage ./mod-utilities.nix {});
       ogfx-tools = (pkgs.callPackage ./ogfx-tools.nix {});
-      ogfx-ui = (pkgs.python39Packages.callPackage ./ogfx-ui.nix { ogfx-tools = self.ogfx-tools; mod-host = self.mod-host; mod-utilities = self.mod-utilities; });
+      ogfx-ui = (pkgs.python39Packages.callPackage ./ogfx-ui.nix {});
       state-variable-filter-lv2 = (pkgs.callPackage ./state-variable-filter-lv2.nix {});
       clipping-lv2 = (pkgs.callPackage ./clipping-lv2.nix {});
     }
