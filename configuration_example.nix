b43670ea2340e7bfd01592aca168173fda23af67
Florian Paul Schmidt <mista.tapas@gmx.net>
2022-09-15 23:11:36 +0200

    add dnsmasq and hostapd, etc..

--- configuration_example.nix
+++ configuration_example.nix
@@ -57,16 +57,30 @@
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   networking = {
+    networkmanager.enable = true;
+    interfaces.wlo1.ipv4.addresses = [ { address = "192.168.150.1"; prefixLength = 24; } ];
     useDHCP = false;
-    interfaces = { 
-      enp1s0.useDHCP = true;
-      enp2s0.useDHCP = true;
-      enp4s0.useDHCP = true;
-      enp5s0.useDHCP = true;
-      wlo1.useDHCP = true;
-    };
+  #   interfaces = { 
+  #     enp1s0.useDHCP = true;
+  #     enp2s0.useDHCP = true;
+  #     enp4s0.useDHCP = true;
+  #     enp5s0.useDHCP = true;
+  #     wlo1.useDHCP = true;
+  #   };
     firewall.enable = false;
     wireguard.enable = true;
+
+    localCommands = ''
+      echo 1 > /proc/sys/net/ipv4/ip_forward
+      ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -o enp1s0 -j MASQUERADE 
+      ${pkgs.iptables}/bin/iptables -A FORWARD -i enp1s0 -o wlo1 -m state --state RELATED,ESTABLISHED -j ACCEPT 
+      ${pkgs.iptables}/bin/iptables -A FORWARD -i wlo1 -o enp1s0 -j ACCEPT
+    '';
+
+    networkmanager.extraConfig = ''
+      [keyfile]
+      unmanaged-devices=interface-name:wlo1
+    '';
   };
 
   i18n.defaultLocale = "en_US.UTF-8";
@@ -77,6 +91,7 @@
       vim wget htop links2 git tmux
       schedtool usbutils psmisc
       lm_sensors
+      wirelesstools
   
       dmenu arandr xfce.xfce4-terminal firefox
       man-pages man-pages-posix
@@ -87,7 +102,31 @@
   
   sound.enable = true;
 
+  environment.etc."dnsmasq-addn-hosts.txt".text =
+    ''
+        192.168.150.1 ogfx86
+    '';
+
   services = {
+    dnsmasq = {
+      enable = true;
+      extraConfig = ''
+        addn-hosts=/etc/dnsmasq-addn-hosts.txt
+        # Bind to only one interface
+        bind-interfaces
+        # Choose interface for binding
+        interface=wlo1
+        # Specify range of IP addresses for DHCP leasses
+        dhcp-range=192.168.150.100,192.168.150.200
+      ''; 
+    };
+
+    hostapd = {
+      enable = true;
+      interface = "wlo1";
+      ssid = "ogfx86";
+      wpaPassphrase = "omg it's fx";
+    };
     openssh.enable = true;
 
     xserver = {
