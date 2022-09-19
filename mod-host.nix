192749e27677c00f0490ffde5c7eb06e44e7f82f
Florian Paul Schmidt <mista.tapas@gmx.net>
2022-09-19 22:53:19 +0200

    bump again

--- mod-host.nix
+++ mod-host.nix
@@ -11,7 +11,7 @@ stdenv.mkDerivation rec {
     repo = "mod-host";
     rev = "prefix";
     # rev = "468c09ae37370139bd79d509b854889a2a567833";
-    sha256 = "sha256-dprndQeTzSsE2y2srwzkZuzU6B7uZnW3zyLT/RSJ9s8=";
+    sha256 = "sha256-K422XnczXuvspREzOj9YGMD0YqKu2u2iGrlvKUnvFTw=";
   };
 
   makeFlags = [ "PREFIX=$(out)" ];
