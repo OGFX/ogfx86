3961db46f8fa44ea327cd3b7c52c3d8399fe4079
Florian Paul Schmidt <mista.tapas@gmx.net>
2022-10-06 09:23:56 +0200

    add acpi=off to disable some more power management

--- boot-kernel-params.nix
+++ boot-kernel-params.nix
@@ -5,5 +5,7 @@
     "nospec_store_bypass_disable" "no_stf_barrier" "mds=off" "tsx=on"
     "tsx_async_abort=off" "mitigations=off" "processor.max_cstate=1"
     "intel_idle.max_cstate=0"
+  ] ++ [
+    "acpi=off"
   ];
 }
