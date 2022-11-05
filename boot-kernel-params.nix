0a2ecea936e0c115957670bbec796c54f6bc7464
Florian Paul Schmidt <mista.tapas@gmx.net>
2022-11-05 13:01:14 +0100

    move kernel params to make-nixos-fast-again module

--- boot-kernel-params.nix
+++ boot-kernel-params.nix
@@ -1,9 +1,6 @@
 { config, pkgs, ...}:
 {
   boot.kernelParams = [
-    "noibrs" "noibpb" "nopti" "nospectre_v2" "nospectre_v1" "l1tf=off"
-    "nospec_store_bypass_disable" "no_stf_barrier" "mds=off" "tsx=on"
-    "tsx_async_abort=off" "mitigations=off" "processor.max_cstate=1"
-    "intel_idle.max_cstate=0"
+    "processor.max_cstate=1" "intel_idle.max_cstate=0"
   ];
 }
