a39bc59c0270133f436c55b43a376519484ad74b
Florian Paul Schmidt <mista.tapas@gmx.net>
2023-04-12 10:31:46 +0200

    add horst

--- ogfx-packages.nix
+++ ogfx-packages.nix
@@ -50,12 +50,13 @@ in
       bollie-delay = (pkgs.callPackage ./pkgs/bolliedelay.nix {});
       mod-pitchshifter = (pkgs.callPackage ./pkgs/mod-pitchshifter.nix {});
       ogfx-tools = (pkgs.callPackage ./pkgs/ogfx-tools.nix {});
-      ogfx-ui = (pkgs.python39Packages.callPackage ./pkgs/ogfx-ui.nix {});
+      ogfx-ui = (pkgs.python3Packages.callPackage ./pkgs/ogfx-ui.nix {});
       state-variable-filter-lv2 = (pkgs.callPackage ./pkgs/state-variable-filter-lv2.nix {});
       clipping-lv2 = (pkgs.callPackage ./pkgs/clipping-lv2.nix {});
       relative_dynamics-lv2 = (pkgs.callPackage ./pkgs/relative_dynamics-lv2.nix {});
       xplugs-lv2 = (pkgs.callPackage ./pkgs/xplugs.nix {});
       vintageac30-lv2 = (pkgs.callPackage ./pkgs/vintageac30.nix {});
+      horst = (pkgs.python3Packages.callPackage ./pkgs/horst.nix {});
       lv2ls_cache =  (pkgs.runCommand "lv2ls_cache" {} ''
         echo out: $out
         mkdir -p $out/share/ogfx_lv2ls_cache
@@ -67,9 +68,9 @@ in
   ];
 
   environment.systemPackages = with pkgs; [
-    mod-host ogfx-tools
+    mod-host ogfx-tools ogfx-ui
     lilv lv2 plugin-torture lv2ls_cache 
-    ladspamm
+    ladspamm horst
   ] ++ plugin_packages;
 
   services.journald.extraConfig = ''
---
+++ pkgs/horst.nix
@@ -0,0 +1,34 @@
+{ lib, stdenv, pkgs, ... }:
+
+stdenv.mkDerivation rec {
+  pname = "horst";
+  version = "0.1";
+
+  # If you have your sources locally, you can specify a path
+  src = /home/fps/src/projects/linux-audio/horst;
+
+  # Pull source from a Git server. Optionally select a specific `ref` (e.g. branch),
+  # or `rev` revision hash.
+  # src = builtins.fetchGit {
+  #   url = "git://github.com/stigok/ruterstop.git";
+  #   ref = "master";
+  #   #rev = "a9a4cd60e609ed3471b4b8fac8958d009053260d";
+  # };
+
+  # Specify runtime dependencies for the package
+  nativeBuildInputs = [ pkgs.python3Packages.pybind11 pkgs.pkg-config pkgs.lilv pkgs.lv2 pkgs.jack2 ];
+
+  PREFIX = "$(out)";
+ 
+  makeFlags = "-j";
+
+  doCheck = false;
+
+  # Meta information for the package
+  meta = {
+    description = ''
+    '';
+  };
+}
+
+
